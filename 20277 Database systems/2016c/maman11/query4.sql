f79f0fc070741ac7266af554e4ddf957abf0f44e
EliArad <mpfmteam@IND-MPFM6.RF-Dynamics.local>
2016-06-29 08:41:45 +0300

    query 5 in manam11

--- 20277 Database systems/2016c/maman11/query4.sql
+++ 20277 Database systems/2016c/maman11/query4.sql
@@ -1,1 +1,16 @@
-﻿sss
+﻿  
+
+select file.fname, file.lname, billing.hours * lawyer.hbilling  as salary from File 
+inner join Billing 
+on file.fid = billing.fid
+inner join lawyer 
+on lawyer.lname = billing.lname
+where Extract(month from file.status) = Extract(month from Billing.bdate)
+order  by salary desc
+limit 1
+
+
+
+
+ 
+ 
---
+++ 20277 Database systems/2016c/maman11/query5.sql
@@ -0,0 +1,19 @@
+﻿select * from (
+select lonfile.lname as lname, count(*) as  toplawyer , lawyer.partner, lawyer.sdate  from lonfile
+inner join lawyer 
+on lawyer.lname = lonfile.lname
+group by lonfile.lname , lawyer.sdate, lawyer.partner) as xxx where  xxx.toplawyer = get_lawyermax()
+
+ 
+
+ CREATE OR REPLACE FUNCTION get_lawyermax()
+  RETURNS integer AS
+$func$
+BEGIN
+
+RETURN (select max(a) from (
+select  count(lname) as a from  lonfile
+group by lname) as xxx);
+
+END
+$func$ LANGUAGE plpgsql;
